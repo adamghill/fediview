a4e0f72a866bd11948e11acd98d3cfcce0a8605e
adamghill <adamghill@yahoo.com>
2023-01-19 18:11:34 -0500

    Add instance and account to admin.

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import Account, Instance, User
+
+admin.site.register(Account)
+admin.site.register(Instance)
 
 admin.site.register(User, UserAdmin)
