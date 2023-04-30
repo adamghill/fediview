8cecdb424f76f04d5f98a3cfebe80ecabb2bdee4
adamghill <adamghill@yahoo.com>
2023-04-30 17:16:44 -0400

    Preload sentence transformers when `activity` app is ready.

---
+++ activity/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "activity"
+
+    def ready(self):
+        if not settings.DEBUG:
+            print("Preload sentence transformer")
+            from activity.text_embeddings_retriever import _get_sentence_transformer
+
+            _get_sentence_transformer()
