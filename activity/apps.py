88e594499e8531875db27fe528d2c4e4e132f53b
adamghill <adamghill@yahoo.com>
2023-05-01 20:45:56 -0400

    Preload sentence transformers again.

--- activity/apps.py
+++ activity/apps.py
@@ -8,8 +8,7 @@ class Config(AppConfig):
 
     def ready(self):
         if not settings.DEBUG:
-            # print("Preload sentence transformer")
-            # from activity.text_embeddings_retriever import _get_sentence_transformer
+            print("Preload sentence transformer")
+            from activity.text_embeddings_retriever import _get_sentence_transformer
 
-            # _get_sentence_transformer()
-            pass
+            _get_sentence_transformer()
