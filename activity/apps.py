5154bf8d83c1c6e0b825cbbd89251be06e30763b
adamghill <adamghill@yahoo.com>
2023-05-11 22:58:07 -0400

    Remove preloading once for all.

--- activity/apps.py
+++ activity/apps.py
@@ -5,10 +5,3 @@ from django.conf import settings
 class Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "activity"
-
-    def ready(self):
-        if not settings.DEBUG:
-            print("Preload sentence transformer")
-            from activity.text_embeddings_retriever import _get_sentence_transformer
-
-            _get_sentence_transformer()
