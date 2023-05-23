fd966172198464003a0f7328782cdeb295ebdeb0
adamghill <adamghill@yahoo.com>
2023-05-22 22:12:59 -0400

    Move sanitize method so it can be scheduled.

--- digest/management/commands/sanitize_generated_emails.py
+++ digest/management/commands/sanitize_generated_emails.py
@@ -8,22 +8,26 @@ from post_office.models import STATUS, Email
 logger = logging.getLogger(__name__)
 
 
+def sanitize():
+    yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+    emails = Email.objects.filter(
+        status=STATUS.sent, last_updated__lte=yesterday
+    ).exclude(Q(message="") & Q(html_message="") & Q(context__isnull=True))
+
+    logger.info(f"Found {len(emails)} emails to sanitize")
+
+    for email in emails:
+        logger.info(f"Sanitize email id {email.id}")
+        email.html_message = ""
+        email.message = ""
+        email.context = None
+        email.save()
+
+    logger.info("Done sanitizing")
+
+
 class Command(BaseCommand):
     help = ""
 
     def handle(self, *args, **options):
-        yesterday = datetime.now(timezone.utc) - timedelta(days=1)
-        emails = Email.objects.filter(
-            status=STATUS.sent, last_updated__lte=yesterday
-        ).exclude(Q(message="") & Q(html_message="") & Q(context__isnull=True))
-
-        logger.info(f"Found {len(emails)} emails to sanitize")
-
-        for email in emails:
-            logger.info(f"Sanitize email id {email.id}")
-            email.html_message = ""
-            email.message = ""
-            email.context = None
-            email.save()
-
-        logger.info("Done sanitizing")
+        sanitize()
