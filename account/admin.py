5beb4cf820ffd8f75cf53476d7d7a443d2869581
adamghill <adamghill@yahoo.com>
2024-10-25 20:12:25 -0400

    Add profile to admin.

--- account/admin.py
+++ account/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Account, Instance, User
+from .models import Account, Instance, Profile, User
 
 admin.site.register(Account)
 admin.site.register(Instance)
+admin.site.register(Profile)
 
 admin.site.register(User, UserAdmin)
